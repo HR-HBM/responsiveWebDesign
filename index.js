c5a30b6afc8c6ca57f54ca321558fab15478eb4a
M. Humaira <132472494+HR-HBM@users.noreply.github.com>
2024-04-17 21:46:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var isColorBlue = false;
+var isColorWhite = false;
 
 function myFunction() {
     var x = document.getElementById("logo");
@@ -11,7 +11,7 @@ function myFunction() {
 
     var navigator = document.querySelector('.bg-body-tertiary');
 
-    if (isColorBlue) {
+    if (isColorWhite) {
       navigator.style.setProperty('background-color', '#6B3CC9', 'important');
   } else {
       navigator.style.setProperty('background-color', 'white', 'important');
@@ -19,7 +19,7 @@ function myFunction() {
 
     var links = document.querySelectorAll('.nav-link');
     links.forEach(function(link) {
-      if (isColorBlue) {
+      if (isColorWhite) {
         link.style.color = 'white';
       } else {
         link.style.color = 'black';
@@ -28,11 +28,11 @@ function myFunction() {
 
     var toggleIcon = document.querySelector('.navbar');
 
-    if (isColorBlue) {
+    if (isColorWhite) {
       toggleIcon.style.setProperty('--bs-navbar-toggler-icon-bg', 'url(\'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-list" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>\')');      
     } else {
       toggleIcon.style.setProperty('--bs-navbar-toggler-icon-bg', 'url(\'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16"><path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/></svg>\')');
       
     }
-    isColorBlue = !isColorBlue;
+    isColorWhite = !isColorWhite;
   }
